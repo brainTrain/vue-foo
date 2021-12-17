6cd180f34715d2045d5f6ce7fba860498fe1b9a6
brainTrain <abductee@gmail.com>
2021-12-16 17:35:38 -0800

    updaetz

--- server/wayback-reverse-proxy.js
+++ server/wayback-reverse-proxy.js
@@ -4,61 +4,73 @@ const {
   WAYBACK_WEB_BASE_URL,
   HTML_FILE_CACHE_DIR,
   JSON_FILE_CACHE_DIR,
-} = require('./constants');
+} = require("./constants");
 
-const fs = require('fs');
-const request = require('request');
-const crypto = require('crypto');
-const formatWaybackTimeseries = require('./format-wayback-timeseries');
-const { parseWaybackRedirect, parseRHBMTopics } = require('./parse-wayback-html');
-const express = require('express');
-const cors = require('cors');
-const through2 = require('through2');
-const requestProxy = require('express-request-proxy');
+const fs = require("fs");
+const request = require("request");
+const crypto = require("crypto");
+const formatWaybackTimeseries = require("./format-wayback-timeseries");
+const {
+  parseWaybackRedirect,
+  parseRHBMTopics,
+} = require("./parse-wayback-html");
+const express = require("express");
+const cors = require("cors");
+const through2 = require("through2");
+const requestProxy = require("express-request-proxy");
 const app = express();
-const http = require('http').Server(app);
+const http = require("http").Server(app);
 
-const PORT = '8000';
+const PORT = "8000";
 
 const CORS_OPTIONS = {
-  origin: 'http://localhost:8080'
+  origin: "http://localhost:8080",
 };
 app.use(cors(CORS_OPTIONS));
 
-app.get('/api/wayback/available', requestProxy({
-  url: `${WAYBACK_BASE_URL}/available`,
-}));
+app.get(
+  "/api/wayback/available",
+  requestProxy({
+    url: `${WAYBACK_BASE_URL}/available`,
+  })
+);
 
-let waybackTimeseriesString = '';
+let waybackTimeseriesString = "";
 const waybackTimeseriesTransform = {
-  name: 'waybackTimeseriesTransform',
-  transform: function () {
+  name: "waybackTimeseriesTransform",
+  transform: function() {
     return through2.obj(
-      function (chunk, encoding, callback) {
+      function(chunk, encoding, callback) {
         const chunkString = chunk.toString();
         waybackTimeseriesString += chunkString;
         callback();
       },
-      function (callback) {
-        const formattedResults = formatWaybackTimeseries(waybackTimeseriesString);
+      function(callback) {
+        const formattedResults = formatWaybackTimeseries(
+          waybackTimeseriesString
+        );
         const { results } = JSON.parse(formattedResults);
+        console.log("results", results);
         this.push(formattedResults);
         // clear tmp value after we've pushed the formatted results
-        waybackTimeseriesString = '';
+        waybackTimeseriesString = "";
         // TODO: figure out best place to set server cache, lazy load on user load? Hmmm 🤔
         handleGetResults(results);
         callback();
       }
-    )
-  }
+    );
+  },
 };
 
-app.get('/api/wayback/web/timemap/link/:siteUrl', requestProxy({
-  url: `${WAYBACK_WEB_BASE_URL}/web/timemap/link/:siteUrl`,
-  transforms: [waybackTimeseriesTransform],
-}));
+app.get(
+  "/api/wayback/web/timemap/link/:siteUrl",
+  requestProxy({
+    url: `${WAYBACK_WEB_BASE_URL}/web/timemap/link/:siteUrl`,
+    transforms: [waybackTimeseriesTransform],
+  })
+);
 
-app.get('/download-url/:url', function (req, res) {
+app.get("/download-url/:url", function(req, res) {
   const { params: { url } } = req;
   const { filepath, isFileSaved } = formatHTMLFilePath(url);
 
@@ -70,13 +82,14 @@ app.get('/download-url/:url', function (req, res) {
   res.send(`ohhhhh: ${url}`);
 });
 
-app.get('/parse-url/:url', function (req, res) {
+// NOTE: I don't remember where this is consumed so I'm doin it in the html file fetch/write handler and created doAJSONWrite lul
+app.get("/parse-url/:url", function(req, res) {
   const { params: { url } } = req;
   const { filepath, isFileSaved } = formatHTMLFilePath(url);
 
   // only write a file if it doesn't exist
   if (!isFileSaved) {
-    getWaybackPage(url, filepath, function (htmlString) {
+    getWaybackPage(url, filepath, function(htmlString) {
       writeJSONFile(htmlString, url);
     });
   } else {
@@ -88,30 +101,48 @@ app.get('/parse-url/:url', function (req, res) {
   res.send(`ohhhhh: ${url}`);
 });
 
-app.get('/topic-json', function (req, res) {
-  const jsonz = getAllTehJSONz();   
+function doAJSONWrite(url) {
+  const { filepath, isFileSaved } = formatHTMLFilePath(url);
+  // only write a file if it doesn't exist
+  if (!isFileSaved) {
+    getWaybackPage(url, filepath, function(htmlString) {
+      writeJSONFile(htmlString, url);
+    });
+  } else {
+    readHTMLFile(filepath, function(htmlString) {
+      writeJSONFile(htmlString, url);
+    });
+  }
+}
+
+app.get("/topic-json", function(req, res) {
+  const jsonz = getAllTehJSONz();
+  console.log("uhhhhhhhhhhhhhhh", jsonz);
   let jsonArray = [];
   let topicsByName = {};
-  jsonz.forEach(function (filename, index) {
+  jsonz.forEach(function(filename, index) {
     const filepath = `${__dirname}${JSON_FILE_CACHE_DIR}/${filename}`;
-    fs.readFile(filepath, 'utf-8', function (error, jsonString) {
+    console.log("filepath", filepath);
+    fs.readFile(filepath, "utf-8", function(error, jsonString) {
       // const jsonObject = JSON.parse(jsonString);
       try {
         const jsonObject = JSON.parse(jsonString);
-        const { userTopicMap } = jsonObject
-        Object.keys(userTopicMap).forEach(function (name) {
+        const { userTopicMap } = jsonObject;
+        Object.keys(userTopicMap).forEach(function(name) {
           topicsByName[name]
-          ? topicsByName[name] = [...topicsByName[name], ...userTopicMap[name]]
-          : topicsByName[name] = userTopicMap[name]
+            ? (topicsByName[name] = [
+                ...topicsByName[name],
+                ...userTopicMap[name],
+              ])
+            : (topicsByName[name] = userTopicMap[name]);
         });
       } catch (error) {
-        console.log('error', error)
+        console.log("error", error);
       }
 
-      jsonArray.push(jsonString)
+      jsonArray.push(jsonString);
 
       if (index === jsonz.length - 1) {
-                
         res.send({
           topicsByName,
         });
@@ -120,35 +151,34 @@ app.get('/topic-json', function (req, res) {
   });
 });
 
-app.get('/topic-json', function (req, res) {
-});
+app.get("/topic-json", function(req, res) {});
 
-function getAllTehJSONz () {
-  const JSON_SOURCES = fs.readdirSync(`${__dirname}${JSON_FILE_CACHE_DIR}`)
+function getAllTehJSONz() {
+  const JSON_SOURCES = fs.readdirSync(`${__dirname}${JSON_FILE_CACHE_DIR}`);
   return JSON_SOURCES;
 }
 
-
-function writeJSONFile (htmlString, url) {
+function writeJSONFile(htmlString, url) {
   const { filepath } = formatJSONFilePath(url);
-  const  RHMBTopics = parseRHBMTopics(htmlString, filepath);
+  const RHMBTopics = parseRHBMTopics(htmlString, filepath);
   writeFile(filepath, JSON.stringify(RHMBTopics));
 }
 
-function readHTMLFile (filepath, callback = function () {}) {
-  fs.readFile(filepath, 'utf-8', function (error, htmlString) {
+function readHTMLFile(filepath, callback = function() {}) {
+  fs.readFile(filepath, "utf-8", function(error, htmlString) {
     callback(htmlString);
   });
 }
 
-function handleGetResults (results) {
+function handleGetResults(results) {
   let stepsSkipped = 0;
-  results.forEach(function (result, index) {
+  results.forEach(function(result, index) {
     const { url } = result;
-    const filename = formatFilename(url, 'html');
+    const filename = formatFilename(url, "html");
     const filepath = formatFilePath(filename, HTML_FILE_CACHE_DIR);
     const fileExists = fs.existsSync(filepath);
-    
+    // console.log("fileExists", fileExists);
+    doAJSONWrite(url);
     // if the file exists, fetch based on timeoutStep variable
     if (!fileExists) {
       // fetch/save every n seconds
@@ -158,20 +188,20 @@ function handleGetResults (results) {
       const timeoutValue = index ? step * timeoutStep : timeoutStep;
       const time = process.hrtime();
 
-      setTimeout(function () {
+      setTimeout(function() {
         handleHtmlFileSave(url);
         // log time delta
-        console.log('time delta:', process.hrtime(time));
+        console.log("time delta:", process.hrtime(time));
       }, timeoutValue);
     } else {
       // keep track of how many steps have been skipped
       // so we can keep our steps even, despite gaps
-      stepsSkipped ++;
+      stepsSkipped++;
     }
   });
 }
 
-function handleHtmlFileSave (url) {
+function handleHtmlFileSave(url) {
   const { filepath, isFileSaved } = formatHTMLFilePath(url);
 
   // only write a file if it doesn't exist
@@ -180,39 +210,42 @@ function handleHtmlFileSave (url) {
   }
 }
 
-function formatJSONFilePath (url) {
-  const filename = formatFilename(url, 'json');
+function formatJSONFilePath(url) {
+  const filename = formatFilename(url, "json");
   const filepath = formatFilePath(filename, JSON_FILE_CACHE_DIR);
   const isFileSaved = fs.existsSync(filepath);
 
-  return { filepath, isFileSaved, }
+  return { filepath, isFileSaved };
 }
 
-function formatHTMLFilePath (url) {
-  const filename = formatFilename(url, 'html');
+function formatHTMLFilePath(url) {
+  const filename = formatFilename(url, "html");
   const filepath = formatFilePath(filename, HTML_FILE_CACHE_DIR);
   const isFileSaved = fs.existsSync(filepath);
 
-  return { filepath, isFileSaved, }
+  return { filepath, isFileSaved };
 }
 
-function formatFilename (url, extension = 'txt') {
+function formatFilename(url, extension = "txt") {
   // hashing url for smaller/friendlier (for software) filenames
-  const urlHash = crypto.createHash('md5').update(url).digest('hex');
+  const urlHash = crypto
+    .createHash("md5")
+    .update(url)
+    .digest("hex");
 
   return `${urlHash}.${extension}`;
 }
 
-function formatFilePath (filename, directory) {
+function formatFilePath(filename, directory) {
   const fileDir = `${__dirname}${directory}`;
 
   return `${fileDir}/${filename}`;
 }
 
-function getWaybackPage (url, filepath, callback = function () {}) {
-  request(url, function (error, response, body) {
+function getWaybackPage(url, filepath, callback = function() {}) {
+  request(url, function(error, response, body) {
     if (error) {
-     return console.log(error)
+      return console.log(error);
     }
 
     const redirectUrl = parseWaybackRedirect(body);
@@ -225,7 +258,7 @@ function getWaybackPage (url, filepath, callback = function () {}) {
   });
 }
 
-function writeFile (filepath, body, callback = function () {}) {
+function writeFile(filepath, body, callback = function() {}) {
   fs.writeFile(filepath, body, function(error) {
     // callback(body);
 
@@ -233,9 +266,9 @@ function writeFile (filepath, body, callback = function () {}) {
       console.log(`tried to write to: ${filepath}`);
       console.log(error);
     }
-  }); 
+  });
 }
 
-http.listen(PORT, function () {
-    console.log(`listening on *: ${PORT}`);
+http.listen(PORT, function() {
+  console.log(`listening on *: ${PORT}`);
 });
